61c3b9846942126d2cab72200c6975f042be983c
SbabalweRoux <sbaviaroux@gmail.com>
2024-05-17 20:25:54 +0200

    Changed styling

--- book-preview.js
+++ book-preview.js
@@ -2,63 +2,68 @@
 
 class BookPreview extends HTMLElement {
     constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+      super();
+      this.attachShadow({ mode: "open" });
     }
-
+  
     connectedCallback() {
-        this.shadowRoot.querySelector('[data-list-close]').addEventListener('click', () => {
-            this.dispatchEvent(new CustomEvent('close'));
-        });
+      this.render();
     }
-
+  
     set book(book) {
-        this._book = book;
-        this.render();
+      this._book = book;
+      this.render();
     }
-
+  
     get book() {
-        return this._book;
+      return this._book;
     }
-
+  
     render() {
-        if (!this._book) return;
-
-        this.shadowRoot.innerHTML = `
-            <style>
-                .preview-container {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    padding: 1rem;
-                    border: 1px solid #ccc;
-                    background-color: #fff;
-                }
-                .preview-image {
-                    max-width: 100%;
-                    height: auto;
-                }
-                .preview-title {
-                    font-size: 1.5rem;
-                    margin: 0.5rem 0;
-                }
-                .preview-author {
-                    font-size: 1rem;
-                    color: #555;
-                }
-                .preview-description {
-                    font-size: 1rem;
-                    margin-top: 0.5rem;
-                }
-            </style>
-            <div class="preview-container">
-                <img class="preview-image" src="${this._book.image}" alt="${this._book.title}">
-                <div class="preview-title">${this._book.title}</div>
-                <div class="preview-author">${this._book.author}</div>
-                <div class="preview-description">${this._book.description}</div>
-            </div>
-        `;
+      if (!this._book) return;
+  
+      this.shadowRoot.innerHTML = `
+              <div class="preview-container">
+                  <img class="preview-image" src="${this._book.image}" alt="${this._book.title}">
+                  <div class="preview-details">
+                      <div class="preview-title">${this._book.title}</div>
+                      <div class="preview-author">${this._book.author}</div>
+                      <div class="preview-description">${this._book.description}</div>
+                  </div>
+              </div>
+              <style>
+                  .preview-container {
+                      display: flex;
+                      padding: 1rem;
+                      border: 1px solid #ccc;
+                      border-radius: 10px;
+                      box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                      background-color: #fff;
+                  }
+                  .preview-image {
+                      max-width: 50%;
+                      height: auto;
+                  }
+                  .preview-details {
+                      margin-left: 1rem;
+                  }
+                  .preview-title {
+                      font-size: 2rem;
+                      font-weight: bold;
+                      margin: 0.5rem 0;
+                  }
+                  .preview-author {
+                      font-size: 1rem;
+                      font-style: italic;
+                      color: #555;
+                  }
+                  .preview-description {
+                      font-size: 0.8rem;
+                      margin-top: 0.5rem;
+                  }
+              </style>
+              
+          `;
     }
 }
-
-customElements.define('book-preview', BookPreview);
+customElements.define("book-preview", BookPreview);
